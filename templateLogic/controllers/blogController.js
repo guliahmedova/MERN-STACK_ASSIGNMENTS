abd6888f0391aa5d0ed265510df6c03ac0a7d279
guluehmedova <guliehmedova19@gmail.com>
2024-05-18 19:54:33 +0400

    template logic

--- templateLogic/controllers/blogController.js
+++ templateLogic/controllers/blogController.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 const blogService = require('../services/blog-service');
 
 const getBlogPage = async (req, res) => {
-    const ejsFilePath = path.join(getRootPath(), "views", "pages" , "blog.ejs");
+    const ejsFilePath = path.join(getRootPath(), "views", "pages", "blog.ejs");
     const blogs = await blogService.getAllBlogs();
 
     fs.readFile(ejsFilePath, "utf-8", (err, data) => {
@@ -25,4 +25,29 @@ const getBlogPage = async (req, res) => {
     });
 };
 
-module.exports = { getBlogPage };
+const getBlogDetailPage = async (req, res) => {
+    const ejsFilePath = path.join(getRootPath(), "views", "pages", "blogDetail.ejs");
+    const blogs = await blogService.getAllBlogs();
+
+    const { url } = req;
+    const id = url.split('/')[3];
+    const blog = blogs?.find((b) => b.id == id);
+
+    fs.readFile(ejsFilePath, "utf-8", (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal server error");
+        };
+
+        const datas = {
+            rootPath: getRootPath(),
+            blog: blog && blog
+        };
+
+        const renderedHtml = ejs.render(data, datas);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(renderedHtml);
+    });
+};
+
+module.exports = { getBlogPage, getBlogDetailPage };
