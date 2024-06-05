3a6c6529b41eb22581919922370787094cc3b3a5
guluehmedova <guliehmedova19@gmail.com>
2024-06-05 19:14:04 +0400

    winston

--- winstonLogging/logging/tg-log.js
+++ winstonLogging/logging/tg-log.js
@@ -1,28 +1,28 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-class TgLogger {
-    constructor() { };
+// class TgLogger {
+//     constructor() { };
 
-    async log(logModel) {
-        console.log('TgLogger');
-        const token = '';
-        const chatId = 0;
+//     async log(logModel) {
+//         console.log('TgLogger');
+//         const token = '';
+//         const chatId = 0;
 
-        const message = JSON.stringify(logModel);
-        const url = `https://api.telegram.org/bot${token}/${chatId}`;
+//         const message = JSON.stringify(logModel);
+//         const url = `https://api.telegram.org/bot${token}/${chatId}`;
 
-        axios.post(url, {
-            chat_id: chatId,
-            text: message,
-            parse_mode: "Markdown"
-        }).then((res) => {
-            if (res.status !== 200) {
-                console.error('Error occured whule sending message to Telegram', res.data);
-            }
-        }).catch(err => {
-            console.error("Error: ", err);
-        });
-    };
-};
+//         axios.post(url, {
+//             chat_id: chatId,
+//             text: message,
+//             parse_mode: "Markdown"
+//         }).then((res) => {
+//             if (res.status !== 200) {
+//                 console.error('Error occured whule sending message to Telegram', res.data);
+//             }
+//         }).catch(err => {
+//             console.error("Error: ", err);
+//         });
+//     };
+// };
 
-module.exports = TgLogger;
+// module.exports = TgLogger;
