6d15e7940d3a607c86ea4eafaf6c48765e9b57cc
guluehmedova <guliehmedova19@gmail.com>
2024-06-05 19:07:42 +0400

    winston

--- logging/logging/slack-log.js
+++ logging/logging/slack-log.js
@@ -1,24 +1,24 @@
-const Slack = require('@slack/bolt');
+// const Slack = require('@slack/bolt');
 
-class SlackLogger {
-    constructor() { };
+// class SlackLogger {
+//     constructor() { };
 
-    // async log(logModel) {
-    //     const token = "xoxb-7201500972726-7201567550630-RthVrKWp5foORZu0ilHAHdC2";
-    //     const secretKey = "488fbc8bec118505d0d84667df5513a5";
-    //     const channel = "ms003";
+//     // async log(logModel) {
+//     //     const token = "xoxb-7201500972726-7201567550630-RthVrKWp5foORZu0ilHAHdC2";
+//     //     const secretKey = "488fbc8bec118505d0d84667df5513a5";
+//     //     const channel = "ms003";
 
-    //     const app = new Slack.App({
-    //         signingSecret: secretKey,
-    //         token: token
-    //     });
+//     //     const app = new Slack.App({
+//     //         signingSecret: secretKey,
+//     //         token: token
+//     //     });
 
-    //     await app.client.chat.postMessage({
-    //         token: token,
-    //         channel: channel,
-    //         text: logModel
-    //     });
-    // };
-};
+//     //     await app.client.chat.postMessage({
+//     //         token: token,
+//     //         channel: channel,
+//     //         text: logModel
+//     //     });
+//     // };
+// };
 
-module.exports = SlackLogger;
+// module.exports = SlackLogger;
